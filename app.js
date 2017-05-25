59ba1486673b8f2c6121f2eb5967fccc551560a6
Yoana <djoana.v@gmail.com>
2017-05-25 22:51:43 +0200

    ha

--- app.js
+++ app.js
@@ -20,21 +20,6 @@ stats.socket.on('error', function (error) {
 //var router = expressPromiseRouter();
 process.env.NODE_ENV = 'test';
 
-// function responseTime(){
-//     return function(req, res, next){
-//         var start = new Date;
-//
-//         if (res._responseTime) return next();
-//         res._responseTime = true;
-//
-//         res.on('header', function(){
-//             var duration = new Date - start;
-//             res.setHeader('X-Response-Time', duration + 'ms');
-//         });
-//
-//         next();
-//     };
-// };
 
 var knex = require('./server/db/knex');
 //app.use(bodyParser());
@@ -76,22 +61,10 @@ var allowCrossDomain = function(req, res, next) {
     next();
 }
 
-// app.all('/*', function(req, res, next) {
-//     // CORS headers
-//     res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//     // Set custom headers for CORS
-//     res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-//     if (req.method == 'OPTIONS') {
-//         res.status(200).end();
-//     } else {
-//         next();
-//     }
-// });
+
 app.use(allowCrossDomain);
 router.use(allowCrossDomain);
-// app.use(responseTime);
-// router.use(responseTime);
+
 router.use("/book/:id",function(req,res,next){
 
     if(req.params.id == 0) {
@@ -119,89 +92,6 @@ router.get("/book/:id",function(req,res,next){
     });
 });
 
-// router.get("/cities/:bookid",function(req,res,next){
-//     knex('city').where('number', req.params.bookid).select()
-//     .then(city => {
-//         //console.log(book);
-//         res.status(200).json({"cities" : city});
-//     })
-//     .catch(error => {
-//         console.error('error: ',error);
-//     });
-// });
-//
-//
-//
-// router.get("/books/:cityName",function(req,res,next){
-//
-//     var subquery = knex('city').where({
-//         city: req.params.cityName
-//     }).select('number');
-//
-//     console.log("The subquery result ",subquery);
-//
-//      knex.select('*').from('book')
-//     .whereIn('number', subquery)
-//     .then(books => {
-//         console.log("The books are " ,books);
-//         res.status(200).json({"books" : books});
-//     })
-//     .catch(error => {
-//         console.error('error: ',error);
-//         return  res.json({
-//             errors: ['Could not get books by city name']
-//         });
-//     });
-// });
-//
-//
-// router.get("/city/:bookTitle",function(req,res,next){
-//     var subquery = knex('book').where({
-//         title: req.params.bookTitle
-//     }).select('number');
-//
-//      knex.select('*').from('city')
-//     .whereIn('number', subquery)
-//     .then(cities => {
-//         // console.log("The cities are " ,cities);
-//         res.status(200).json({"cities" : cities});
-//     })
-//     .catch(error => {
-//         console.error('error: ',error);
-//        return res.json({
-//             errors: ['Could not get cities by book title']
-//     });
-// });
-// });
-//
-// router.get("/allInfo/:author",function(req,res,next){
-//
-//     knex('book').select(['book.title', 'city.city', 'city.lat', 'city.longt'])
-//     .innerJoin('city', 'city.number', 'book.number')
-//     .then(books => {
-//         res.status(200).json({"books" :  books});
-//     })
-//     .catch(error => {
-//         console.error('error: ',error);
-//         return res.json({
-//             errors: ['Could not get cities by book title']
-//         });
-//    });
-// });
-//
-// router.get("/rangeBooks/:range/:range2",function(req,res,next){
-//
-//     knex('book').whereBetween('number', [req.params.range, req.params.range2])
-//     .then(books => {
-//         res.status(200).json({"books" :  books});
-//     })
-//     .catch(error => {
-//         console.error('error: ',error);
-//         return res.json({
-//             errors: ['Could not get cities by book title']
-//         });
-//     });
-// });
 
 
 router.put("/bookUpdate/:bookTitle",function(req,res,next){
@@ -219,162 +109,7 @@ router.put("/bookUpdate/:bookTitle",function(req,res,next){
 
 
 });
-//
-// router.put("/citiesUpdate/:bookTitle/:cityName",function(req,res,next){
-//
-//      knex.select('number')
-//     .from('book')
-//     .where({title: req.params.bookTitle})
-//     .then(function(rows){
-//         return _.map(rows,'number')
-//     })
-//     .then(function(numbers){
-//         numbers.map(num => {
-//             // return knex.update({city:req.param.cityName})
-//             return  knex('city')
-//             .where({number: num})
-//             .update({city: req.params.cityName})
-//              .then(function(){
-//                 res.send("Successfully updated City!!!Bravo Joji!!!!")
-//             })
-//             .catch(function(error){
-//                 console.error(error);
-//             })
-//         });
-//
-//     })
-//     .catch(function(error){
-//         console.error(error);
-//         res.send(err);
-//     });
-//
-// });
-//
-// router.delete("/citiesDelete/:bookAuthor",function(req,res,next){
-//      knex.select('number')
-//     .from('book')
-//     .where({author: req.params.bookAuthor})
-//      .then(function(rows){
-//          return _.map(rows,'number')
-//          // _.pluck(rows, 'name');
-//      })
-//     .then(function(rows){
-//        // res.json("Book's Author Updated!!!");
-//          console.log('The rows are ',rows);
-//          rows.map(row => {
-//           //   console.log("The num is ",row);
-//              return knex('city').where({number:row}).del()
-//              .catch(function(error){
-//                  console.error(error);
-//              })
-//          });
-//          res.send("Successfully deleted!!!Bravo Joji!!!!")
-//     })
-//     .catch(function(error){
-//         console.error(error);
-//         res.send(err);
-//     });
-//
-// });
-//
-// router.post("/bookInput/:bookAuthor/:cityName",function(req,res,next){
-//
-//     knex.select('number')
-//     .from('book')
-//     .where({author: req.params.bookAuthor})
-//     .andWhere('number' , '<' , 1000)
-//     .then(function(rows){
-//         return _.map(rows,'number')
-//         // _.pluck(rows, 'name');
-//     })
-//     .then(function(numbers){
-//         // return knex.insert({number: numbers}).into('city')
-//         //knex.map(function(number){})
-//         //console.log("The numbers ",numbers);
-//         numbers.map(num => {
-//             console.log("The num is ",num);
-//             return knex.insert({number:num,city:req.params.cityName},'id').into('city')
-//             .catch(function(error){
-//                 console.error(error);
-//             })
-//         });
-//     })
-//     .then(function(id){
-//         console.log('Inserted ids ' + id);
-//         res.send("Successfully inserted!!!Bravo Joji!!!!")
-//     })
-//     //})
-//     .catch(function(error){
-//         console.error(error);
-//     });
-//
-// });
-//
-// router.post("/cityInputByBookTitle/:bookTitle",function(req,res,next){
-//
-//     knex.select('number')
-//     .from('book')
-//     .where({title: req.params.bookTitle})
-//     .then(function(rows){
-//         return _.map(rows,'number')
-//         // _.pluck(rows, 'name');
-//     })
-//     .then(function(numbers){
-//         // return knex.insert({number: numbers}).into('city')
-//         //knex.map(function(number){})
-//         //console.log("The numbers ",numbers);
-//         numbers.map(num => {
-//            // console.log("The num is ",num);
-//             return knex.insert({number:num,city:req.body.city,lat:req.body.lat,longt:
-//             req.body.longt},'id').into('city')
-//             .catch(function(error){
-//                 console.error(error);
-//             })
-//         });
-//     })
-//     .then(function(id){
-//         console.log('Inserted ids ' + id);
-//         res.send("Successfully inserted City!!!Bravo Joji!!!!")
-//     })
-//     //})
-//     .catch(function(error){
-//         console.error(error);
-//     });
-//
-// });
-//
-// router.post("/cityInputByBookAuthor/:bookAuthor",function(req,res,next){
-//
-//     knex.select('number')
-//     .from('book')
-//     .where({author: req.params.bookAuthor})
-//     .then(function(rows){
-//         return _.map(rows,'number')
-//         // _.pluck(rows, 'name');
-//     })
-//     .then(function(numbers){
-//         // return knex.insert({number: numbers}).into('city')
-//         //knex.map(function(number){})
-//         //console.log("The numbers ",numbers);
-//         numbers.map(num => {
-//             // console.log("The num is ",num);
-//             return knex.insert({number:num,city:req.body.city,lat:req.body.lat,longt:
-//             req.body.longt},'id').into('city')
-//             .catch(function(error){
-//                 console.error(error);
-//             })
-//         });
-//     })
-//     .then(function(id){
-//         console.log('Inserted ids ' + id);
-//         res.send("Successfully inserted City!!!Bravo Joji!!!!")
-//     })
-//     //})
-//     .catch(function(error){
-//         console.error(error);
-//     });
-//
-// });
+
 
 router.post("/bookPost", function(req,res,next){
 //console.log('The body is ',req.body);
@@ -390,104 +125,6 @@ router.post("/bookPost", function(req,res,next){
 });
 
 
-
-
-// router.post("/cityInput/:bookAuthor/:cityName",function(req,res,next){
-//
-//      knex.select('number')
-//      .from('book')
-//      .where({author: req.params.bookAuthor})
-//      .andWhere('number' , '<' , 1000)
-//      .then(function(rows){
-//          return _.map(rows,'number')
-//         // _.pluck(rows, 'name');
-//      })
-//       .then(function(numbers){
-//          // return knex.insert({number: numbers}).into('city')
-//          //knex.map(function(number){})
-//          console.log("The numbers ",numbers);
-//          numbers.map(num => {
-//              console.log("The num is ",num);
-//             return knex.insert({number:num,city:req.params.cityName},'id').into('city')
-//              .catch(function(error){
-//                  console.error(error);
-//             })
-//          });
-//
-//        })
-//          .then(function(id){
-//              console.log('Inserted ids ' + id);
-//            res.send("Successfully inserted!!!Bravo Joji!!!!")
-//          })
-//       //})
-//        .catch(function(error){
-//            console.error(error);
-//        });
-//
-// });
-
-
-
-
-
-
-//
-//     knex('book').whereBetween('number', [req.params.range, req.params.range2])
-//     .then(books => {
-//         res.status(200).json({"books" :  books});
-//     })
-//     .catch(error => {
-//         console.error('error: ',error);
-//         return res.json({
-//             errors: ['Could not get cities by book title']
-//         });
-//     });
-// });
-
-
-// Handle 404 error.
-// The last middleware.
-// app.use("*",function(req,res){
-//     res.status(404).send('404');
-// });
-
-// const router = express.Router();
-// process.env.NODE_ENV = 'test';
-// const knex = require('./server/db/knex');
-// Define the port to run on
-
-//app.set('port', 5000);
-
-//app.use('/static', express.static(__dirname, '/public/index.html'));
-//////////app.use('/', express.static(__dirname + '/public'));
-//console.log(__dirname);
-// Listen for requests
-//////////////
-// var books = require('./server/routes/books');
- //app.use('/books', books);
-//////////////////
-// app.get('/books/:quesNum', (req, res, next) => {
-//     var quesNum = req.param('quesNum');
-//     console.log(quesNum);
-//     // knex.select('*').from('book_city')
-//     //     .whereIn('book_id',function(){
-//     //     this.select('id').from('book').whereRaw('title = ');
-//     knex.raw('select * from users where id in (select id from book where title =(?)', [quesNum]);
-//
-//   })
-// .then((cities) => {
-//     res.status(200).json({
-//         status: 'success',
-//         data: cities,
-//       });
-//   })
-// .catch((err) => {
-//     res.status(500).json({
-//         status: 'error',
-//         data: err,
-//       });
-//   });
-//});
 
 app.use("/api",router);
 router.use(function(req, res, next) {
@@ -509,16 +146,6 @@ if (app.get('env') === 'development') {
 }
 
 
-// production error handler
-// no stacktraces leaked to user
-//
-// router.use(function(err, req, res, next) {
-//         res.status(err.status || 500)
-//         .json({
-//             status: 'error',
-//             message: err.message
-//         });
-//     });
 
 router.use(function(err, req, res, next) {
     res.status(err.status || 500)
@@ -531,9 +158,6 @@ router.use(function(err, req, res, next) {
 router.use(function(req, res) { var time = Date.now() - req.start; });
 app.use(function(req, res) { var time = Date.now() - req.start; });
 
-// var server = app.listen(port, function () {
-//     var port = server.address().port;
-//     console.log('Magic happens on port ' + port);
-//   });
+
 
 module.exports = app;
